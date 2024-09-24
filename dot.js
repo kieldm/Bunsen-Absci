2489b5f3ef6571f6a3459a74cb5fe51f99bf6f07
Kiel M <kiel.d.m@gmail.com>
2024-09-24 17:27:21 -0400

    Launch Absci DotGrid

---
+++ dot.js
@@ -0,0 +1,101 @@
+class Dot {
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+
+    this.c = col[0];
+
+    this.decaySpeed = 1;
+    this.colorCulm = 0;
+
+    this.sprayDist = 250;
+    this.sprayAmount = 6;
+
+    this.drawDist = spacer * 2;
+    this.drawAmount = 20;
+  }
+
+  run(){
+    // this.noiseAdd();
+    this.update();
+    this.display();
+  }
+
+  update(){
+    if(this.colorCulm < 1){
+      this.c = col[0];
+    } else if(this.colorCulm < 50){
+      var tk0 = map(this.colorCulm, 1, 50, 0, 1);
+      this.c = lerpColor(col[0], col[1], tk0);
+    } else if(this.colorCulm < 100){
+      var tk0 = map(this.colorCulm, 50, 100, 0, 1);
+      this.c = lerpColor(col[1], col[2], tk0);
+    } else if(this.colorCulm < 150){
+      var tk0 = map(this.colorCulm, 100, 150, 0, 1);
+      this.c = lerpColor(col[2], col[3], tk0);
+    } else {
+      this.c = col[3];
+    }
+
+    if(this.colorCulm > 0){
+      this.colorCulm -= this.decaySpeed;
+    } else {
+      this.colorCulm = 0;
+    }
+  }
+
+  display(){
+    push();
+      translate(this.x, this.y);
+
+      fill(this.c);
+      ellipse(0, 0, size, size);
+    pop();
+  }
+
+  noiseAdd(){
+    var tk0 = noise((this.x + frameCount * 2) * 0.005, (this.y + frameCount * 2) * 0.005);
+    var tk1 = map(tk0, 0, 1, 0, 2);
+
+    if(this.colorCulm < 150){
+      this.colorCulm += tk1;
+    }
+  }
+
+  mouseAdd(){
+    var thisDist = dist(mouseX, mouseY, this.x, this.y)
+    if(thisDist < this.sprayDist){
+      var tk0 = map(thisDist, 0, this.sprayDist, 0, 1);
+      var tk1 = easeInOutQuad(tk0);
+      var tk2 = map(tk1, 0, 1, this.sprayAmount, 0);
+
+      if(this.colorCulm < 150){
+        this.colorCulm += tk2;
+      }
+    }
+
+    var thisDist = dist(mouseX, mouseY, this.x, this.y)
+    if(thisDist < this.drawDist){
+      var tk0 = map(thisDist, 0, this.drawDist, 0, 1);
+      var tk1 = easeInOutSine(tk0);
+      var tk2 = map(tk1, 0, 1, this.drawAmount, 0);
+
+      if(this.colorCulm < 150){
+        this.colorCulm += tk2;
+      }
+    }
+  }
+
+  mousePressedAdd(){
+    var thisDist = dist(mouseX, mouseY, this.x, this.y)
+    if(thisDist < this.sprayDist * 2){
+      var tk0 = map(thisDist, 0, this.sprayDist * 2, 0, 1);
+      var tk1 = easeInOutQuad(tk0);
+      var tk2 = map(tk1, 0, 1, this.sprayAmount * 10, 0);
+
+      if(this.colorCulm < 150){
+        this.colorCulm += tk2;
+      }
+    }
+  }
+}
---
+++ sketch_dotGrid.js
@@ -0,0 +1,70 @@
+var bkgdColor, foreColor;
+var col = [];
+
+let spacer = 20;
+let xCount, yCount;
+let xSpacer, ySpacer;
+let size = 8;
+
+let dots = [];
+
+function preload(){
+  tFont = loadFont("resources/Inter-Regular.ttf");
+}
+
+function setup(){
+  createCanvas(windowWidth,windowHeight);
+
+  rectMode(CENTER);
+
+  xCount = round(width/spacer);
+  yCount = round(height/spacer);
+
+  xSpacer = width/xCount;
+  ySpacer = height/yCount;
+
+  xCount++;
+  yCount++;
+
+  // SKITTLE
+  bkgdColor = color('#000000');
+  foreColor = color('#ffffff');  
+  col[0] = color('#000000');
+  col[1] = color('#454545');
+  col[2] = color('#cacaca');
+  col[3] = color('#FF472E');
+
+  for(var m = 0; m < yCount; m++){
+    for(var n = 0; n < xCount; n++){
+      dots[dots.length] = new Dot(n * xSpacer, m * ySpacer);
+    }
+  }
+
+  console.log("THIS MANY DOTS: " + dots.length);
+}
+
+function draw() {
+  background(bkgdColor);
+
+  for(var p = 0; p < dots.length; p++){
+    dots[p].run();
+  }
+
+  // fill(foreColor);
+  // noStroke();
+  // textSize(20);
+  // textFont(tFont)
+  // text(round(frameRate()), 50, height - 50);
+}
+
+function mouseMoved(){
+  for(var p = 0; p < dots.length; p++){
+    dots[p].mouseAdd();
+  }
+}
+
+function mousePressed(){
+  for(var p = 0; p < dots.length; p++){
+    dots[p].mousePressedAdd();
+  }
+}
